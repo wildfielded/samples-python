afe35d34e7b43b195699f15dad71658469cf031c
Ded Wildfield <wildfielded@gmail.com>
2022-01-17 18:37:07 +0800

    Workday save 17.01.2022

--- YLabTrain1/main.py
+++ YLabTrain1/main.py
@@ -1,8 +1,10 @@
-post = {'id': 0, 'coords': (0, 2), 'descr': 'Почтовое отделение'}
-point1 = {'id': 1, 'coords': (2, 5), 'descr': 'Ул. Грибоедова, 104/25'}
-point2 = {'id': 2, 'coords': (5, 2), 'descr': 'Ул. Бейкер стрит, 221б'}
-point3 = {'id': 3, 'coords': (6, 6), 'descr': 'Ул. Большая Садовая, 302-бис'}
-point4 = {'id': 4, 'coords': (8, 3), 'descr': 'Вечнозелёная Аллея, 742'}
+#!/usr/bin/python3
+
+post = {'id': 0, 'coords': (0.0, 2.0), 'descr': 'Почтовое отделение'}
+point1 = {'id': 1, 'coords': (2.0, 5.0), 'descr': 'Ул. Грибоедова, 104/25'}
+point2 = {'id': 2, 'coords': (5.0, 2.0), 'descr': 'Ул. Бейкер стрит, 221б'}
+point3 = {'id': 3, 'coords': (6.0, 6.0), 'descr': 'Ул. Большая Садовая, 302-бис'}
+point4 = {'id': 4, 'coords': (8.0, 3.0), 'descr': 'Вечнозелёная Аллея, 742'}
 
 point_list = [post, point1, point2, point3, point4]
 point_set = set([point_['id'] for point_ in point_list[1:]])
@@ -35,16 +37,22 @@ draft_routes = [
 ]
 
 def create_distance_array(point_list_):
-    calc_distance = lambda a, b : ((a['coords'][0] - b['coords'][0]) ** 2 + (a['coords'][1] - b['coords'][1]) ** 2) ** 0.5
+    ''' Создаёт диагонально-симметричную матрицу всех расстояний между всеми
+        точками. Расстояние от A до B и расстояние от B до A вычисляется один раз.
+        То есть, как положено, для 5 точек вычисляется 4+3+2+1=10 расстояний.
+    '''
+    calc_dist = lambda a, b : ((a['coords'][0] - b['coords'][0]) ** 2 + (a['coords'][1] - b['coords'][1]) ** 2) ** 0.5
     len_ = len(point_list_)
-    distance_array = [[0 for col_ in range(len_)] for raw_ in range(len_)]
+    dist_array_ = [[0 for col_ in range(len_)] for raw_ in range(len_)]
     for start_point in point_list_:
         i_ = start_point['id']
         for finish_point in point_list_[i_+1:]:
             j_ = finish_point['id']
-            distance_array[i_][j_] = distance_array[j_][i_] = calc_distance(point_list_[i_], point_list_[j_])
-    return distance_array
+            dist_array_[i_][j_] = dist_array_[j_][i_] = calc_dist(point_list_[i_], point_list_[j_])
+    return dist_array_
 
-distances = create_distance_array(point_list)
-for k_ in distances:
-    print(k_)
+
+if __name__ == '__main__':
+    distance_array = create_distance_array(point_list)
+    for k_ in distance_array:
+        print(k_)
