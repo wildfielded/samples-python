a9e83981b2675fd572f8bf77fa074fd22de65c1f
Ded Wildfield <wildfielded@gmail.com>
2022-01-18 18:06:58 +0800

    Workday save 18-01-2022

--- YLabTrain1/main.py
+++ YLabTrain1/main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+#####=====----- Variables -----=====#####
+
 post = {'id': 0, 'coords': (0.0, 2.0), 'descr': 'Почтовое отделение'}
 point1 = {'id': 1, 'coords': (2.0, 5.0), 'descr': 'Ул. Грибоедова, 104/25'}
 point2 = {'id': 2, 'coords': (5.0, 2.0), 'descr': 'Ул. Бейкер стрит, 221б'}
@@ -8,33 +10,10 @@ point4 = {'id': 4, 'coords': (8.0, 3.0), 'descr': 'Вечнозелёная Ал
 
 point_list = [post, point1, point2, point3, point4]
 point_set = set([point_['id'] for point_ in point_list[1:]])
+all_routes = []
 
-draft_routes = [
-    [1,2,3,4],
-    [1,2,4,3],
-    [1,3,2,4],
-    [1,3,4,2],
-    [1,4,2,3],
-    [1,4,3,2],
-    [2,1,3,4],
-    [2,1,4,3],
-    [2,3,1,4],
-    [2,3,4,1],
-    [2,4,1,3],
-    [2,4,3,1],
-    [3,1,2,4],
-    [3,1,4,2],
-    [3,2,1,4],
-    [3,2,4,1],
-    [3,4,1,2],
-    [3,4,2,1],
-    [4,1,2,3],
-    [4,1,3,2],
-    [4,2,1,3],
-    [4,2,3,1],
-    [4,3,1,2],
-    [4,3,2,1]
-]
+
+#####=====----- Functions -----=====#####
 
 def create_distance_array(point_list_):
     ''' Создаёт диагонально-симметричную матрицу всех расстояний между всеми
@@ -45,14 +24,55 @@ def create_distance_array(point_list_):
     len_ = len(point_list_)
     dist_array_ = [[0 for col_ in range(len_)] for raw_ in range(len_)]
     for start_point in point_list_:
-        i_ = start_point['id']
-        for finish_point in point_list_[i_+1:]:
-            j_ = finish_point['id']
-            dist_array_[i_][j_] = dist_array_[j_][i_] = calc_dist(point_list_[i_], point_list_[j_])
+        s_ = start_point['id']
+        for finish_point in point_list_[s_+1:]:
+            f_ = finish_point['id']
+            dist_array_[s_][f_] = dist_array_[f_][s_] = calc_dist(point_list_[s_], point_list_[f_])
     return dist_array_
 
+def multiply_list(set_: set={}, list_: list=[]):
+    ''' Размножает список на элементы, в которых последовательно (с применением
+        генератора) к исходному списку добавляется один член из множества.
+        Выводит список полученных списков.
+    '''
+    output_list_ = []
+    for item_ in (s_ for s_ in set_):
+        output_list_.append(list_ + [item_])
+    return output_list_
+
+def find_routes(set_: set={}, list_: list=[]):
+    ''' Создаёт список всех возможных последовательностей (маршрутов) посещения
+        точек по их номерам (id). Каждый маршрут тоже в виде списка без начальной
+        и конечной точки id=0 (почты). Используется рекурсия, результат заносится
+        в глобальную переменную all_routes.
+    '''
+    result_list_ = []
+    if len(list_) > 0:
+        if len(list_[0]) < len(set_):
+            tmp_list_ = []
+            for id_list_ in list_:
+                tmp_set_ = set_.copy()
+                for id_ in id_list_:
+                    tmp_set_.remove(id_)
+                tmp_list_ += multiply_list(tmp_set_, id_list_)
+            result_list_ = tmp_list_
+            find_routes(set_, result_list_)
+        else:
+            global all_routes
+            all_routes = list_
+    else:
+        result_list_ += multiply_list(set_)
+        find_routes(set_, result_list_)
+
+
+#####=====----- MAIN -----=====#####
 
 if __name__ == '__main__':
     distance_array = create_distance_array(point_list)
     for k_ in distance_array:
         print(k_)
+    find_routes(point_set)
+    print(all_routes)
+    #####print(len(all_routes))
+
+#####=====----- THE END -----=====########################################
