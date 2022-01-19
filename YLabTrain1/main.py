b89cffca269c3c34ad985fedd62c72b1c18ad6c8
Ded Wildfield <wildfielded@gmail.com>
2022-01-19 17:45:45 +0800

    ItWorks variant 19-01-2022

--- YLabTrain1/main.py
+++ YLabTrain1/main.py
@@ -2,15 +2,16 @@
 
 #####=====----- Variables -----=====#####
 
-post = {'id': 0, 'coords': (0.0, 2.0), 'descr': 'Почтовое отделение'}
-point1 = {'id': 1, 'coords': (2.0, 5.0), 'descr': 'Ул. Грибоедова, 104/25'}
-point2 = {'id': 2, 'coords': (5.0, 2.0), 'descr': 'Ул. Бейкер стрит, 221б'}
-point3 = {'id': 3, 'coords': (6.0, 6.0), 'descr': 'Ул. Большая Садовая, 302-бис'}
-point4 = {'id': 4, 'coords': (8.0, 3.0), 'descr': 'Вечнозелёная Аллея, 742'}
+post = {'id': 0, 'coords': (0, 2), 'descr': 'Почтовое отделение'}
+point1 = {'id': 1, 'coords': (2, 5), 'descr': 'Ул. Грибоедова, 104/25'}
+point2 = {'id': 2, 'coords': (5, 2), 'descr': 'Ул. Бейкер стрит, 221б'}
+point3 = {'id': 3, 'coords': (6, 6), 'descr': 'Ул. Большая Садовая, 302-бис'}
+point4 = {'id': 4, 'coords': (8, 3), 'descr': 'Вечнозелёная Аллея, 742'}
 
 point_list = [post, point1, point2, point3, point4]
 point_set = set([point_['id'] for point_ in point_list[1:]])
 all_routes = []
+ALLOWED_DELTA = 0.01
 
 
 #####=====----- Functions -----=====#####
@@ -23,10 +24,10 @@ def create_distance_array(point_list_):
     calc_dist = lambda a, b : ((a['coords'][0] - b['coords'][0]) ** 2 + (a['coords'][1] - b['coords'][1]) ** 2) ** 0.5
     len_ = len(point_list_)
     dist_array_ = [[0 for col_ in range(len_)] for raw_ in range(len_)]
-    for start_point in point_list_:
-        s_ = start_point['id']
-        for finish_point in point_list_[s_+1:]:
-            f_ = finish_point['id']
+    for start_point_ in point_list_:
+        s_ = start_point_['id']
+        for finish_point_ in point_list_[s_+1:]:
+            f_ = finish_point_['id']
             dist_array_[s_][f_] = dist_array_[f_][s_] = calc_dist(point_list_[s_], point_list_[f_])
     return dist_array_
 
@@ -64,15 +65,73 @@ def find_routes(set_: set={}, list_: list=[]):
         result_list_ += multiply_list(set_)
         find_routes(set_, result_list_)
 
+def calc_distance(route_list_, distance_array_):
+    ''' Для данного маршрутного листа суммирует расстояния из матрицы расстояний,
+        добавляя расстояния от почты до первой и последней точки маршрута.
+    '''
+    distance_ = (distance_array_[0][route_list_[0]] + distance_array_[0][route_list_[-1]])
+    for d_ in range(len(route_list_) - 1):
+        distance_ += distance_array_[route_list_[d_]][route_list_[d_+1]]
+    return distance_
+
+def get_shorty(route_list_, point_list_, distance_array_, allowed_delta_):
+    ''' Находит кратчайший маршрут. Дополнительно находит маршруты, близкие к
+        к нему по расстоянию (квадрат разницы меньше заданной дельты).
+    '''
+    min_route_ = []
+    min_dist_ = 0.0
+    for route_ in route_list_:
+        length_ = calc_distance(route_, distance_array_)
+        if min_dist_:
+            if length_ < min_dist_:
+                min_dist_ = length_
+                min_route_ = [route_]
+        else:
+            min_dist_ = length_
+    for route_ in route_list_:
+        if route_ != min_route_[0]:
+            length_ = calc_distance(route_, distance_array_)
+            if (length_ - min_dist_) ** 2 < allowed_delta_:
+                min_route_.append(route_)
+    return min_route_
+
+def write_string(list_, point_list_, distance_array_):
+    ''' Из маршрутного листа формирует строку в требуемом формате
+    '''
+    dist_sum_ = 0.0
+    list_ = [0] + list_ + [0]
+    output_str_ = str(point_list_[0]['coords'])
+    for r_ in range(len(list_) - 2):
+        dist_sum_ += distance_array_[list_[r_]][list_[r_+1]]
+        p_ = str(point_list_[list_[r_+1]]['coords'])
+        d_ = str(dist_sum_)
+        output_str_ += ' -> {}[{}]'.format(p_, d_)
+    dist_sum_ += distance_array_[list_[-2]][0]
+    p_ = str(point_list_[0]['coords'])
+    d_ = str(dist_sum_)
+    output_str_ += ' -> {}[{}] = {}'.format(p_, d_, d_)
+    return output_str_
+
+def output_result(short_list_, point_list_, distance_array_):
+    ''' Вывод результата работы программы с некоторым расширением к заданию.
+        А именно: дополнительно выводит альтернативные маршруты (если есть),
+        близкие по расстоянию к кратчайшему.
+    '''
+    output_str_ = u'Кратчайший маршрут:\n'
+    output_str_ += write_string(short_list_[0], point_list_, distance_array_)
+    if len(short_list_) > 1:
+        output_str_ += u'\nАльтернативные кратчайшие маршруты:\n'
+        for another_list_ in short_list_[1:]:
+            output_str_ += write_string(another_list_, point_list_, distance_array_)
+    return output_str_
+
 
 #####=====----- MAIN -----=====#####
 
 if __name__ == '__main__':
     distance_array = create_distance_array(point_list)
-    for k_ in distance_array:
-        print(k_)
     find_routes(point_set)
-    print(all_routes)
-    #####print(len(all_routes))
+    short_list = get_shorty(all_routes, point_list, distance_array, ALLOWED_DELTA)
+    print(output_result(short_list, point_list, distance_array))
 
 #####=====----- THE END -----=====########################################
