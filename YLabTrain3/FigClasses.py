0fa0423a8a22ea4ada0894d376da41499091b57b
Ded Wildfield <wildfielded@gmail.com>
2022-02-16 18:29:20 +0800

    Workday Fix2 16-02-2022

--- YLabTrain3/FigClasses.py
+++ YLabTrain3/FigClasses.py
@@ -1,102 +1,250 @@
 #!/usr/bin/python3
 
-#####=====----- Variables -----=====#####
+from math import pi as pi_
 
-PI = 3.141592653589793
-
-#####=====----- Classes -----=====#####
-
-class SymFigure():
-    base_category = 'Симметричная фигура'
-    image = None
 
 class Fig2D():
-    class_dimension = 'Плоская фигура'
+    ''' Класс двумерных фигур с общими методами всех фигур
+    '''
+    figure_dimension = 'Плоская фигура'
+
+    @classmethod
+    def perimeter(cls):
+        return None
+
+    @classmethod
+    def square(cls):
+        return None
+
+    @staticmethod
+    def image(path_to_img):
+        return '/web/' + path_to_img
 
 class Fig3D():
-    class_dimension = 'Объёмная фигура'
+    ''' Класс трёхмерных фигур с общими методами всех фигур
+    '''
+    figure_dimension = 'Объёмная фигура'
 
-class Circle(SymFigure, Fig2D):
+    @classmethod
+    def volume(cls):
+        return None
+
+    @staticmethod
+    def image(path_to_img):
+        return '/web/' + path_to_img
+
+#####=====----- Круг и наследники -----=====#####
+
+class Circle(Fig2D):
     figure_name = 'Круг'
-    def __init__(self):
-        super(SymFigure).__init__()
-        super(Fig2D).__init__()
-    def perimeter(self, r_):
-        return 2 * PI * r_
-    def square(self, r_):
-        return PI * (r_ ** 2)
+    img_file = 'circle.png'
 
-class Triangle(SymFigure, Fig2D):
-    figure_name = 'Равнобедренный треугольник'
-    def __init__(self):
-        super(SymFigure).__init__()
-        super(Fig2D).__init__()
-    def perimeter(self):
-        return ((ab_ ** 2 + h_ ** 2) ** 0.5) * 2 + ab_
-    def square(self, ab_, h_):
-        return (ab_ * h_) / 2
-
-class Trapezoid(SymFigure, Fig2D):
-    figure_name = 'Трапеция'
-    def __init__(self):
-        super(SymFigure).__init__()
-        super(Fig2D).__init__()
-    def perimeter(self, a_, b_, h_):
-        return a_ + b_ + (((a_ - b_) ** 2) + h_ ** 2) ** 0.5
-    def square(self, a_, b_, h_):
-        return ((a_ + b_) * h_) / 2
-
-class Rectangle(SymFigure, Fig2D):
-    ''' Прямоугольник
-    '''
     def __init__(self):
         super().__init__()
 
-class Quadro(Fig2D):
-    ''' Квадрат
-    '''
-    def __init__(self):
-        super().__init__()
+    @classmethod
+    def perimeter(cls, r_):
+        return 2 * pi_ * r_
 
-class Rhombus(Fig2D):
-    ''' Ромб
-    '''
-    def __init__(self):
-        super().__init__()
+    @classmethod
+    def square(cls, r_):
+        return pi_ * (r_ ** 2)
+
 
 class Sphere(Circle):
-    ''' Сфера
-    '''
+    figure_name = 'Сфера'
+    img_file = 'sphere.png'
+
     def __init__(self):
-        super(Circle).__init__()
-    def square(self, r_):
-        return 4 * PI * (r_ ** 2)
-    def volume(self, r_):
-        return (4 / 3) * PI * (r_ ** 3)
+        super().__init__()
 
-class Cube():
-    pass
+    @classmethod
+    def square(cls, r_):
+        return 4 * pi_ * (r_ ** 2)
 
-class Parallelepiped():
-    pass
+    @classmethod
+    def volume(cls, r_):
+        return (4 / 3) * pi_ * (r_ ** 3)
 
-class Pyramid3F():
-    pass
 
-class Pyramid4F():
-    pass
+class Cylinder(Circle):
+    figure_name = 'Цилиндр'
+    img_file = 'cylinder.png'
 
-class Cylinder():
-    pass
+    def __init__(self):
+        super().__init__()
 
-class Cone():
-    pass
+    @classmethod
+    def square(cls, r_, h_):
+        return 2 * pi_ * r_ * (h_ + r_)
 
-#####=====----- Functions -----=====#####
+    @classmethod
+    def volume(cls, r_, h_):
+        return pi_ * (r_ ** 2) * h_
+
+
+class Cone(Circle):
+    figure_name = 'Конус'
+    img_file = 'cone.png'
+
+    def __init__(self):
+        super().__init__()
+
+    @classmethod
+    def square(cls, r_, h_):
+        return pi_ * (r_ ** 2) + (pi_ * r_ * (r_ ** 2 + h_ ** 2) ** 0.5)
+
+    @classmethod
+    def volume(cls, r_, h_):
+        return pi_ * (r_ ** 2) * h_ / 3
+
+
+#####=====----- Треугольник и наследники -----=====#####
+
+class Triangle(Fig2D):
+    figure_name = 'Равнобедренный треугольник'
+    img_file = 'triangle.png'
+
+    def __init__(self):
+        super().__init__()
+
+    @classmethod
+    def perimeter(cls, a_, h_):
+        return ((a_ ** 2 + h_ ** 2) ** 0.5) * 2 + a_
+
+    @classmethod
+    def square(cls, a_, h_):
+        return (a_ * h_) / 2
+
+
+class Pyramid3F(Triangle):
+    figure_name = 'Тетраэдр'
+    img_file = 'pyramid3f.jpg'
+
+    def __init__(self):
+        super().__init__()
+
+    @classmethod
+    def volume(cls, a_, h_, height_):
+        return height_ * (a_ * h_) / 6
+
+
+#####=====----- Квадрат и наследники -----=====#####
+
+class Quadro(Fig2D):
+    figure_name = 'Квадрат'
+    img_file = 'quadro.png'
+
+    def __init__(self):
+        super().__init__()
+
+    @classmethod
+    def perimeter(cls, a_):
+        return a_ * 4
+
+    @classmethod
+    def square(cls, a_, h_):
+        return a_ ** 2
+
+
+class Rhombus(Quadro):
+    figure_name = 'Ромб'
+    img_file = 'rhombus.png'
+
+    def __init__(self):
+        super().__init__()
+
+    @classmethod
+    def square(cls, d1_, d2_):
+        return d1_ * d2_ / 2
+
+
+class Cube(Quadro):
+    figure_name = 'Куб'
+    img_file = 'cube.png'
+
+    def __init__(self):
+        super().__init__()
+
+    @classmethod
+    def square(cls, a_):
+        return (a_ ** 2) * 6
+
+    @classmethod
+    def volume(cls, a_):
+        return a_ ** 3
+
+
+class Pyramid4F(Quadro):
+    figure_name = 'Пирамида'
+    img_file = 'pyramid4f.png'
+
+    def __init__(self):
+        super().__init__()
+
+    @classmethod
+    def volume(cls, a_, h_):
+        return (a_ ** 2) * h_ / 3
+
+
+class Rectangle(Quadro):
+    figure_name = 'Прямоугольник'
+    img_file = 'rectangle.png'
+
+    def __init__(self):
+        super().__init__()
+
+    @classmethod
+    def perimeter(cls, a_, h_):
+        return 2 * (a_ + h_)
+
+    @classmethod
+    def square(cls, a_, h_):
+        return a_ * h_
+
+
+class Parallelepiped(Rectangle):
+    figure_name = 'Параллелепипед'
+    img_file = 'parallelepiped.png'
+
+    def __init__(self):
+        super().__init__()
+
+    @classmethod
+    def square(cls, a_, b_, h_):
+        return ((a_ * b_) + (a_ * h_) + (b_ * h_)) * 2
+
+    @classmethod
+    def volume(cls, a_, b_, h_):
+        return a_ * b_ * h_
+
+
+#####=====----- Трапеция и наследники -----=====#####
+
+class Trapezoid(Fig2D):
+    figure_name = 'Трапеция'
+    img_file = 'trapezoid.png'
+
+    def __init__(self):
+        super().__init__()
+
+    @classmethod
+    def perimeter(cls, a_, b_, h_):
+        return a_ + b_ + (((a_ - b_) ** 2) + h_ ** 2) ** 0.5
+
+    @classmethod
+    def square(cls, a_, b_, h_):
+        return ((a_ + b_) * h_) / 2
+
 
 #####=====----- MAIN -----=====#####
 
 if __name__ == '__main__':
-    pass
+    ttt = Sphere()
+    print(ttt.figure_name)
+    print(str(ttt.perimeter(3)))
+    print(str(ttt.square(3)))
+    print(str(ttt.volume(3)))
+    print(ttt.image(ttt.img_file))
 
 #####=====----- THE END -----=====########################################
--- YLabTrain3/main.py
+++ YLabTrain3/main.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
+from string import Template
+
 from bottle import route, run, request, redirect, HTTPError
 
 import FigClasses as fc_
+import HTML_Templates as ht_
 
 #####=====----- Variables -----=====#####
 
@@ -12,9 +15,7 @@ import FigClasses as fc_
 
 @route('/')
 def server_root():
-    with open('web/index.html', 'r', encoding='utf-8') as f_:
-        output_ = f_.read()
-    return output_
+    return ht_.HEADER + ht_.FORM_INI + ht_.FOOTER
 
 @route('/figure', method='POST')
 def figure():
@@ -24,7 +25,37 @@ def figure():
 
 @route('/figure/<class_>')
 def figure_class(class_):
-    return 'It works'
+    if class_ == 'Quadro':
+        new_form_ = ht_.FORM_QUADRO
+    if class_ == 'Cone':
+        new_form_ = ht_.FORM_CONE
+    if class_ == 'Circle':
+        new_form_ = ht_.FORM_CIRCLE
+    if class_ == 'Cube':
+        new_form_ = ht_.FORM_CUBE
+    if class_ == 'Parallelepiped':
+        new_form_ = ht_.FORM_PARALLELEPIPED
+    if class_ == 'Pyramid3F':
+        new_form_ = ht_.FORM_PYRAMID3F
+    if class_ == 'Pyramid4F':
+        new_form_ = ht_.FORM_PYRAMID4F
+    if class_ == 'Rectangle':
+        new_form_ = ht_.FORM_RECTANGLE
+    if class_ == 'Rhombus':
+        new_form_ = ht_.FORM_RHOMBUS
+    if class_ == 'Sphere':
+        new_form_ = ht_.FORM_SPHERE
+    if class_ == 'Trapezoid':
+        new_form_ = ht_.FORM_TRAPEZOID
+    if class_ == 'Triangle':
+        new_form_ = ht_.FORM_TRIANGLE
+    if class_ == 'Cylinder':
+        new_form_ = ht_.FORM_CYLINDER
+    return ht_.HEADER + new_form_ + ht_.FOOTER
+
+@route('/result', method='POST')
+def result(**kwargs):
+    return ht_.HEADER + ht_.FOOTER
 
 #####=====----- MAIN -----=====#####
 
