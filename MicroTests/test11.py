54f826dc3d3c4fcc7c8fa43e4b8b21330e697af7
Ded Wildfield <wildfielded@gmail.com>
2023-01-08 19:43:11 +0800

    MicroTest upd3 08-01-2023     new file:   MicroTests/test11.py     - Solving Test 1.1     modified:   MicroTests/test12.py     - Some cosmetic fix

---
+++ MicroTests/test11.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import re
+
+def domain_name(url_str: str) -> str:
+    ''' Returns the "effective" domain name from URL string
+    Arguments:
+        url_str [str] -- URL string
+    Returns:
+        [str] -- Effective domain name
+    '''
+    subdom_list = ['co', 'com', 'org', 'edu', 'gov']
+    doms_list = url_str.replace('http://', '')\
+                       .replace('https://', '')\
+                       .split('/', maxsplit=1)[0]\
+                       .split('.')
+    return doms_list[-2] if doms_list[-2] not in subdom_list else doms_list[-3]
+
+''' =====----- MAIN -----===== '''
+if __name__ == '__main__':
+    assert domain_name("http://google.com") == "google"
+    assert domain_name("http://google.co.jp") == "google"
+    assert domain_name("www.xakep.ru") == "xakep"
+    assert domain_name("https://youtube.com") == "youtube"
+    assert domain_name('http://github.com/carbonfive/raygun') == "github"
+
+#####=====----- THE END -----=====#########################################
--- MicroTests/test12.py
+++ MicroTests/test12.py
@@ -5,11 +5,11 @@ def int32_to_ip(int32_: int) -> str:
     Arguments:
         int32_n [int] -- Decimal integer number
     Returns:
-        ip_str [str] -- IP-address in dotted-decimal notation
+        [str] -- IP-address in dotted-decimal notation
     '''
     ip_str = ''
     for pow_ in range(3, 0, -1):
-        ip_str += str(int32_ // (256**pow_)) + '.'
+        ip_str += str(int32_ // (256 ** pow_)) + '.'
         int32_ %= 256 ** pow_
     return ip_str + str(int32_)
 
