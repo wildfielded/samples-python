2982f7657ea435e2fb41603af5da769a5281831a
Ded Wildfield <wildfielded@gmail.com>
2023-01-09 22:56:51 +0800

    MicroTest upd3 09-01-2023

---
+++ MicroTests/test21.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+class CyclicIterator:
+    def __iter__(self):
+        pass
+
+    def __next__(self):
+        pass
+
+''' =====----- MAIN -----===== '''
+if __name__ == '__main__':
+    pass
+
+#####=====----- THE END -----=====#########################################
