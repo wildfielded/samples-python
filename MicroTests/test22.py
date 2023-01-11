f316e1dd758819a282b6cc6e0325d648b4ba5c8a
Ded Wildfield <wildfielded@gmail.com>
2023-01-11 17:30:33 +0800

    MicroTest upd1 11-01-2023

---
+++ MicroTests/test22.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Generator, List, Tuple
+
+
+@dataclass
+class Movie:
+    title: str
+    dates: List[Tuple[datetime, datetime]]
+
+    def schedule(self) -> Generator[datetime, None, None]:
+        return []
+
+
+''' =====----- MAIN -----===== '''
+if __name__ == '__main__':
+    m = Movie('sw', [
+        (datetime(2020, 1, 1), datetime(2020, 1, 7)),
+        (datetime(2020, 1, 15), datetime(2020, 2, 7))
+    ])
+    for d in m.schedule():
+        print(d)
+
+#####=====----- THE END -----=====#########################################
