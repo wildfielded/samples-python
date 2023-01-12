9521bc905d6ada2db5981256ebe9deed7acd4416
Ded Wildfield <wildfielded@gmail.com>
2023-01-12 21:15:24 +0800

    MicroTest upd1 12-01-2023

---
+++ MicroTests/test31.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def multiplier(number: int):
+    return number * 2
+
+''' =====----- MAIN -----===== '''
+if __name__ == '__main__':
+    pass
+
+#####=====----- THE END -----=====#########################################
